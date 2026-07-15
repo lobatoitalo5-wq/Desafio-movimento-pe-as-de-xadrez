d986fc08b92bac40e19c78ffdd700897654589bc
Italobato <lobatoitalo5@gmail.com>
2026-07-15 12:45:04 -0300

    add movimento do cavalo com loops aninhados

--- mov_peca_xadrez.c
+++ mov_peca_xadrez.c
@@ -19,5 +19,24 @@ int main(){
             printf("Rainha para Esquerda\n"); // imprime "Rainha para Esquerda" 8 vezes
             R++;
         }
+
+    // mover peça cavalo
+    // possiveis movimentos do cavalo e suas variaveis:
+    // 1. Duas casas para frente e uma para direita = 2FD
+    // 2. Duas casas para frente e uma para esquerda = 2FE
+    // 3. Duas casas para trás e uma para direita = 2TD
+    // 4. Duas casas para trás e uma para esquerda = 2TE
+    // 5. Uma casa para frente e duas para direita = 1FD
+    // 6. Uma casa para frente e duas para esquerda = 1FE
+    // 7. Uma casa para trás e duas para direita = 1TD
+    // 8. Uma casa para trás e duas para esquerda = 1TE
+            
+        int movimentoCavalo = 1;
+        while (movimentoCavalo--){
+            for (int i=0; i<2; i++) {
+                printf("Cavalo para frente\n"); // imprime "Cavalo para frente" 2 vezes
+        }
+            printf("Cavalo para direita\n"); // imprime "Cavalo para direita" 1 vez
+        }
     return 0;   
-    }
+}
